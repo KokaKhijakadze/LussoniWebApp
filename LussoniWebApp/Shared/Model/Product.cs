55b55ddfb50512ed5111dbadc9f77d0d02e95dd8
Koka Khijakadze <k.khijakadze@gmail.com>
2021-12-02 16:06:14 +0400

    Added Employee.cs, Entity.cs and Product.cs

---
+++ LussoniWebApp/Shared/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace LussoniWebApp.Shared.Model;
+
+public class Product : Entity
+{
+    public string? ProductName { get; set; }
+    public decimal ProductPrice { get; set; }
+    public int ProductQuantity { get; set; }
+
+}
