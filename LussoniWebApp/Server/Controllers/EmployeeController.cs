c51aa2e16220b7fd248b729b1d36915eb76b65fa
Koka Khijakadze <k.khijakadze@gmail.com>
2021-12-03 13:34:10 +0400

    Added EmployeeController.cs
    This controller will be used for Employee

---
+++ LussoniWebApp/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using LussoniWebApp.Shared.Data;
+using LussoniWebApp.Shared.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LussoniWebApp.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    public readonly ApplicationDbContext dbContext;
+    public EmployeeController(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Employee>>> Get()
+    {
+        return await dbContext.Employees.ToListAsync();
+
+    }
+    [HttpPost]
+    public async Task<ActionResult> Post(Employee employee)
+    {
+        dbContext.Add(employee);
+        await dbContext.SaveChangesAsync();
+        return new CreatedAtRouteResult("GetEmployee", new { id = employee.ID }, employee);
+        
+    }
+    [HttpPut]
+    public async Task<ActionResult> Put(Employee employee)
+    {
+        dbContext.Entry(employee).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var employee = new Employee { ID = id };
+        dbContext.Remove(employee);
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
